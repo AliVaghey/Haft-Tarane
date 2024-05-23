2f2538f32792489b98af0b6f192b02cfbad137a3
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-05-23 11:20:37 +0330

    send documents

--- app/[lang]/(panel)/agency/tours/[id]/edit/documents/page.jsx
+++ app/[lang]/(panel)/agency/tours/[id]/edit/documents/page.jsx
@@ -3,7 +3,7 @@
 import { Input } from "@/components/ui/input";
 import useMount from "@/hooks/use-mount";
 import { routes } from "@/routes/routes";
-import { usePathname, useRouter } from "next/navigation";
+import { useParams, usePathname, useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -38,31 +38,27 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import ToastSuccess from "@/components/toast/toast-success";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  documentSchema,
+  endocumentSchema,
+} from "@/lib/validation/tour/document";
 
 const DocumentPage = () => {
   const dictionary = useDictionary();
   const mount = useMount();
   const router = useRouter();
+  const params = useParams();
 
   const form = useForm({
     resolver: zodResolver(
-      dictionary["language"] === "fa"
-        ? basicInformationSchema
-        : enBasicInformationSchema,
+      dictionary["language"] === "fa" ? documentSchema : endocumentSchema,
     ),
     defaultValues: {
-      title: "",
-      trip_type: "",
-      expiration: "",
-      selling_type: "",
-      tour_styles: null,
-      capacity: "",
-      evening_support: false,
-      midnight_support: false,
-      origin: "",
-      destination: "",
-      staying_nights: "",
-      transportation_type: "",
+      free_services: [],
+      certificates: [],
+      descriptions: "",
+      cancel_rules: "",
     },
     mode: "onSubmit",
   });
@@ -78,57 +74,32 @@ const DocumentPage = () => {
   } = form;
 
   const onSubmit = async (values) => {
-    console.log("valuesssssss", `{${values.tour_styles.join(",")}}`);
-    const {
-      title,
-      trip_type,
-      expiration,
-      selling_type,
-      tour_styles,
-      capacity,
-      evening_support,
-      midnight_support,
-      origin,
-      destination,
-      staying_nights,
-      transportation_type,
-    } = values;
+    const { free_services, certificates, descriptions, cancel_rules } = values;
 
     await CSRFToken();
 
     const encodedFormData = querystring.stringify({
-      title,
-      trip_type,
-      expiration,
-      selling_type,
-      tour_styles: JSON.stringify(tour_styles),
-      capacity,
-      evening_support: evening_support ? "1" : "0",
-      midnight_support: midnight_support ? "1" : "0",
-      origin,
-      destination,
-      staying_nights,
-      transportation_type,
+      tour_id: params.id,
+      free_services: JSON.stringify(free_services),
+      certificates: JSON.stringify(certificates),
+      descriptions,
+      cancel_rules,
     });
 
-    console.log("encodedFormData", encodedFormData);
+    console.log("values", values);
 
     await axios
-      .post("/api/agency/tour", encodedFormData)
+      .post("/api/agency/tour/certificates", encodedFormData)
       .then((response) => {
-        console.log("draft-response", response.data);
+        console.log("certificates-response", response.data);
 
-        if (response.status === 201) {
-          toast.success(
-            <ToastSuccess text={"پیش نویس تور با موفقیت ایجاد شد"} />,
-          );
-          router.push(
-            routes.agency.tours.edit["travel-plans"](response.data.data.id),
-          );
+        if (response.status === 204) {
+          toast.success(<ToastSuccess text={"مدارک با موفقیت اضافه شدند"} />);
+          router.push(routes.agency.tours.edit.hotels(params.id));
         }
       })
       .catch((error) => {
-        console.log("draft-error", error);
+        console.log("certificates-error", error);
         toast.error(
           <ToastError
             text={
@@ -151,213 +122,19 @@ const DocumentPage = () => {
           <div className="grid w-full grid-cols-1 gap-2 lg:grid-cols-2">
             <FormField
               control={control}
-              name="origin"
-              render={({ field }) => (
-                <FormItem className="col-span-3 text-right lg:col-span-1">
-                  <FormLabel>مبدا</FormLabel>
-                  <SearchableSelect
-                    changeValue={(value) => {
-                      field.onChange(value);
-                    }}
-                    api={"/api/agency/hotels"}
-                    placeholder={"مبدا"}
-                  />
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="destination"
-              render={({ field }) => (
-                <FormItem className="col-span-3 text-right lg:col-span-1">
-                  <FormLabel>مبدا</FormLabel>
-                  <SearchableSelect
-                    changeValue={(value) => {
-                      field.onChange(value);
-                    }}
-                    api={"/api/agency/hotels"}
-                    placeholder={"مقصد"}
-                  />
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="title"
+              name="free_services"
               render={({ field }) => (
                 <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>عنوان</FormLabel>
-                  <FormControl>
-                    <Input
-                      className=""
-                      autoComplete="off"
-                      placeholder="حداقل ۲ کاراکتر"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="trip_type"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>نوع تور</FormLabel>
-                  <FormControl>
-                    <Input
-                      className=""
-                      autoComplete="off"
-                      placeholder="حداقل ۲ کاراکتر"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="selling_type"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>نوع پرداخت</FormLabel>
-                  <FormControl>
-                    <Input
-                      className=""
-                      autoComplete="off"
-                      placeholder="نوع پرداخت"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="staying_nights"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>تعداد شب</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      className=""
-                      autoComplete="off"
-                      placeholder="تعداد شب"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="capacity"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>ظرفیت</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      className=""
-                      autoComplete="off"
-                      placeholder="ظرفیت"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={control}
-              name="expiration"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>روز های باقی مانده</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      className=""
-                      autoComplete="off"
-                      placeholder="روز های باقی مانده"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <div className="flex items-center justify-center gap-3 rounded-lg border p-2">
-              <FormField
-                control={form.control}
-                name="evening_support"
-                render={({ field }) => (
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="pb-2">
-                      <FormLabel className="cursor-pointer">
-                        پشتیبانی شبانه
-                      </FormLabel>
-                    </div>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="midnight_support"
-                render={({ field }) => (
-                  <FormItem className="flex items-center gap-2">
-                    <FormControl>
-                      <Checkbox
-                        checked={field.value}
-                        onCheckedChange={field.onChange}
-                      />
-                    </FormControl>
-                    <div className="pb-2">
-                      <FormLabel className="cursor-pointer">
-                        پشتیبانی نیمه شب
-                      </FormLabel>
-                    </div>
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            <FormField
-              control={control}
-              name="tour_styles"
-              render={({ field }) => (
-                <FormItem className="col-span-3 lg:col-span-1">
-                  <FormLabel>مدل های تور</FormLabel>
+                  <FormLabel>سرویس های رایگان</FormLabel>
                   <FormControl>
                     <ChipInput
-                      initialData={getValues("tour_styles")}
-                      placeholder="امکانات"
+                      initialData={getValues("free_services")}
+                      placeholder="تایپ کنید..."
                       onChange={(data) => {
                         console.log("data", data);
-                        setValue("tour_styles", data);
+                        setValue("free_services", data, {
+                          shouldValidate: true,
+                        });
                       }}
                     />
                   </FormControl>
@@ -367,28 +144,60 @@ const DocumentPage = () => {
             />
 
             <FormField
-              control={form.control}
-              name="transportation_type"
+              control={control}
+              name="certificates"
               render={({ field }) => (
-                <FormItem>
-                  <FormLabel>نوع حمل و نقل</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="نوع حمل و نقل" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent>
-                      <SelectItem value="system">سیستم</SelectItem>
-                      <SelectItem value="self">شخصی</SelectItem>
-                      <SelectItem value="none">هیچ کدام</SelectItem>
-                    </SelectContent>
-                  </Select>
-
+                <FormItem className="col-span-3 lg:col-span-1">
+                  <FormLabel>مدارک همراه</FormLabel>
+                  <FormControl>
+                    <ChipInput
+                      initialData={getValues("certificates")}
+                      placeholder="تایپ کنید..."
+                      onChange={(data) => {
+                        console.log("data", data);
+                        setValue("certificates", data, {
+                          shouldValidate: true,
+                        });
+                      }}
+                    />
+                  </FormControl>
                   <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="descriptions"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>توضیحات</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="توضیحات"
+                      className="h-32 resize-y focus-visible:ring-primary"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-primary" />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={control}
+              name="cancel_rules"
+              render={({ field }) => (
+                <FormItem className="">
+                  <FormLabel>قوانین کنسلی</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="قوانین کنسلی"
+                      className="h-32 resize-y focus-visible:ring-primary"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-primary" />
                 </FormItem>
               )}
             />
