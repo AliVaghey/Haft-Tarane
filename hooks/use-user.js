cc09e8117a8fa1025b8962af761c5a56c91b755b
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-06-02 12:13:39 +0330

    special tours

--- hooks/use-user.js
+++ hooks/use-user.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export const useUser = create((set) => ({
   banners: [],
   setBanners: (data) => set({ banners: data }),
+  specialTours: [],
+  setSpecialTours: (data) => set({ specialTours: data }),
   userData: false,
   isLogin: false,
   setUserData: (data) => set({ userData: data, isLogin: true }),
