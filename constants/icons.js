7bf7f7c0762c44e33ca80e61275be9019f852bd4
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-06-05 10:20:19 +0330

    add icons

---
+++ constants/icons.js
@@ -0,0 +1,19 @@
+import airplane1 from "@/public/icons/airplane-1.png";
+import airplane2 from "@/public/icons/airplane-2.png";
+import calendar from "@/public/icons/calendar.png";
+import degree from "@/public/icons/degree.png";
+import mapPin from "@/public/icons/map-pin.png";
+import sailboat from "@/public/icons/sailboat.png";
+import train from "@/public/icons/train.png";
+import user from "@/public/icons/user.png";
+
+export {
+  airplane1,
+  airplane2,
+  calendar,
+  degree,
+  mapPin,
+  sailboat,
+  train,
+  user,
+};
