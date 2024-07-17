b816ca50698141939788719aa7613ba001356804
MahdiArabpour <85918072+MahdiArabpour98@users.noreply.github.com>
2024-07-17 11:11:27 +0330

    set to draft

--- app/[lang]/(panel)/agency/tours/all/components/cell-action.jsx
+++ app/[lang]/(panel)/agency/tours/all/components/cell-action.jsx
@@ -2,18 +2,19 @@
 
 import { routes } from "@/routes/routes";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import SubmitButton from "@/components/submit-button";
 import { useState } from "react";
 import { CircleCheckBig } from "lucide-react";
 import { CSRFToken, axios } from "@/lib/axios";
 import { toast } from "sonner";
+import { useTour } from "@/hooks/use-tour";
 
 const CellAction = ({ data }) => {
-  const router = useRouter();
+  const tourHook = useTour();
 
   const [loading, setLoading] = useState(false);
+  const [loading2, setLoading2] = useState(false);
 
   const onCopy = async () => {
     try {
@@ -21,7 +22,7 @@ const CellAction = ({ data }) => {
 
       await CSRFToken();
 
-      const response = await axios.post(`api/agency/tour/${data.id}/copy`);
+      const response = await axios.post(`/api/agency/tour/${data.id}/copy`);
 
       console.log("response", response.status);
 
@@ -43,13 +44,43 @@ const CellAction = ({ data }) => {
     }
   };
 
+  const onDraft = async () => {
+    try {
+      setLoading2(true);
+
+      await CSRFToken();
+
+      const response = await axios.put(`/api/agency/tour/${data.id}/draft`);
+
+      console.log("response", response.status);
+
+      if (response.status === 204) {
+        toast.success(
+          <div className="flex items-center gap-2">
+            <span>
+              <CircleCheckBig className="text-green-600" />
+            </span>
+            <span>{"تور به حالت پیش نویس تغییر یافت"}</span>
+          </div>,
+        );
+
+        tourHook.setFlag(!tourHook.flag);
+      }
+    } catch (error) {
+      console.log("error", error);
+      toast.error("مشکلی پیش آمده است. لطفا مجددا تلاش فرمایید");
+    } finally {
+      setLoading2(false);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center">
       <div className="flex items-center gap-2">
         <Link href={routes.agency.tours.details(data.id)}>
           <Button
             variant="ghost"
-            className="h-8 rounded-3xl border border-red-dark text-red-dark"
+            className="h-7 rounded-3xl border border-red-dark text-xs text-red-dark"
           >
             جزئیات تور
           </Button>
@@ -57,11 +88,20 @@ const CellAction = ({ data }) => {
 
         <SubmitButton
           variant="ghost"
-          className="h-8 rounded-3xl border border-blue-500 text-blue-500"
+          className="h-7 rounded-3xl border border-green-500 text-xs text-green-500"
           loading={loading}
           onClick={onCopy}
         >
           کپی تور
+        </SubmitButton>
+
+        <SubmitButton
+          variant="ghost"
+          className="h-7 rounded-3xl border border-blue-500 text-xs text-blue-500"
+          loading={loading2}
+          onClick={onDraft}
+        >
+          پیش نویس
         </SubmitButton>
       </div>
     </div>
